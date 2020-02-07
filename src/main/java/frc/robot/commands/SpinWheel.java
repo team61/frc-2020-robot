26ecba1ab4d7c1452f12cb25f55c4559313eae5d
Kevin Downing <kevindowning.personal@gmail.com>
2020-02-06 20:20:36 -0500

    Added SpinWheel commands

---
+++ src/main/java/frc/robot/commands/SpinWheel.java
@@ -0,0 +1,26 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.WheelSpinnerConstants;
+import frc.robot.subsystems.WheelSpinner;
+
+public class SpinWheel extends CommandBase {
+
+    private WheelSpinner m_wheelSpinner;
+
+    public SpinWheel(WheelSpinner wheelSpinner) {
+        m_wheelSpinner = wheelSpinner;
+
+        addRequirements(wheelSpinner);
+    }
+
+    @Override
+    public void execute() {
+        m_wheelSpinner.setVoltage(WheelSpinnerConstants.kWheelVoltage);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_wheelSpinner.stop();
+    }
+}
